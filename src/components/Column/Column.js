d9bf917f907a602cdf1c0efe7e9e462460e3c6f4
Jakub Churski <churskijakub@gmail.com>
2022-03-14 14:56:30 +0100

    Card generator from Redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,14 +5,14 @@ import { useSelector } from 'react-redux';
 
 
 const Column = props => {
-    const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id);;
+    const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id);
     return ( 
         <article className={styles.column}><span className={styles.icon + ' fa fa-' + props.icon} />
-        <h2 className={styles.title}>{props.text}</h2>
+        <h2 className={styles.title}>{props.title}</h2>
         <ul className={styles.cards}>
         {cards.map(card => <Card key={card.id} title={card.title} /> )}
         </ul>
-        <CardForm columnId={props.id} action={props.addCard} />
+        <CardForm columnId={props.id}/>
         </article>
     );
 };
